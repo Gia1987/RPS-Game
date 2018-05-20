482875692f4f871334002bbff1b81f041257d8d0
Giacomo <giacomo.n27@gmail.com>
2018-05-20 14:55:23 +0200

    Create Index.erb and implemented app.rb file

--- app.rb
+++ app.rb
@@ -1,8 +1,10 @@
 require 'sinatra/base'
+# to run the app type : $ rerun -- rackup --port 4000 config.ru (for Windows)
 
 class RPS < Sinatra::Base
+  enable :sessions
 
   get '/' do
-    "Welcome to RPS Game!"
-  end
+    erb(:index)
+  ends
 end
