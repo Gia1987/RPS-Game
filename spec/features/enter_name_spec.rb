5af1598e8a1e10bfe5341169f4561e9cceb36a9a
Giacomo <giacomo.n27@gmail.com>
2018-05-22 22:43:20 +0200

    Refactored test

--- spec/features/enter_name_spec.rb
+++ spec/features/enter_name_spec.rb
@@ -5,6 +5,12 @@ feature 'Nick name' do
   scenario 'player can enter own name' do
     sign_in
     expect(page).to have_content("Welcome Giacomo")
+  end
+end
+
+feature 'weapon available' do
+  scenario 'player can choice own weapon' do
+    sign_in
     expect(page).to have_button('Rock')
     expect(page).to have_button('Paper')
     expect(page).to have_button('Scissors')
