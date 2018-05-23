317ba25128c049a2f156a38e4a85b8c5ed7df58b
Giacomo <giacomo.n27@gmail.com>
2018-05-23 16:23:47 +0200

    Rename "result" class to "game", implemented create "score" method and tested

---
+++ lib/game.rb
@@ -0,0 +1,30 @@
+class Game
+  attr_reader :player_score, :pc_score
+
+  def initialize
+    @player_score = 0
+    @pc_score = 0
+    @res = 'Is a Draw'
+  end
+
+  def result(player, pc)
+    if player == pc
+      @res = 'Is a Draw'
+    elsif player == 'Rock' && pc == 'Paper' ||
+          player == 'Paper' && pc == 'Scissors' ||
+          player == 'Scissors' && pc == 'Rock'
+      @res = 'You Lose'
+    else
+      @res = 'You Won'
+    end
+    return @res
+  end
+
+  def score
+    if @res == 'You Lose'
+      @pc_score += 1
+    elsif @res == 'You Won'
+      @player_score += 1
+    end
+  end
+end
---
+++ spec/game_spec.rb
@@ -0,0 +1,26 @@
+require './spec/spec_helper'
+require './lib/game'
+
+describe Game do
+  subject(:game) {described_class.new}
+
+  context '#result' do
+    it 'returns the winner' do
+      #arrange
+      player = 'Rock'
+      pc_weapon = 'Paper'
+      #assert
+      expect(game.result(player,pc_weapon)).to eq 'You Lose'
+    end
+  end
+
+  context '#score' do
+    it'increment player_score and pc_score' do
+      #arrange
+      game.result('Rock','Paper')
+      game.score
+      #assert
+      expect(game.pc_score).to eq 1
+    end
+  end
+end
